79ea4b04b27d7e6137ecc799b643d895eff138df
jakirmithunbd <jakirmithunbd@gmail.com>
2018-01-18 15:22:17 +0600

    edacity

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+
+
+(function ($) {
+	"use strict";
+
+	$('#simple-menu').sidr({
+    	timing: 'ease-in-out',
+    	speed: 500
+  	});
+	
+
+
+}(jQuery));
+
+      
