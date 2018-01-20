a4a8cc7c6361c7b76994603c823c620598295889
rubelmah55 <rubelmah55@gmail.com>
2018-01-20 13:30:41 +0600

    add all file now

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php if (get_field('favicon', 'options')): ?>
         <link rel="icon" href="<?php the_field('favicon', 'options'); ?>" sizes="32x32">
         <?php endif; ?>
